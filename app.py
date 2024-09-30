da3efdf12e23b5028008ccb4e23e009c48aed003
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2024-09-30 20:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+#pip install flask
+from flask import Flask, render_template
+import calendar
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+    return render_template('index.html')
+#if __name__== '__main__':
+    #app.run(debug=True)
 
-#if __name__ == "__main__":
-	#app.run()
+if __name__== '__main__':
+    app.run(debug=True, port=8080)
